b0430079de8c0a7e645e8efd2b2ccbf44dc4d5e1
christopheveloupoule <christopheveloupoule@yahoo.fr>
2020-10-27 21:15:10 +0100

    Conso API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 import React from "react";
-import Navbar from "./components/Navbar.js";
-import QuoteAPI from "./components/QuoteAPI";
+import QuoteCard from "./components/QuoteCard";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <QuoteAPI />
-    </div>
-  );
+import axios from "axios";
+
+const simpsonsQuote = {
+  quote: "By chilling my loins I increase the chances of impregnating my wife.",
+  character: "Apu Nahasapeemapetilon",
+  image:
+    "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567511629"
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      simpsons: simpsonsQuote
+    };
+    this.getSimpsons = this.getSimpsons.bind(this);
+  }
+
+  getSimpsons() {
+    // Send the request
+    axios
+      .get("https://thesimpsonsquoteapi.glitch.me/quotes")
+      // Extract the DATA from the received response
+      .then((response) => response.data)
+      // Use this data to update the state
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          simpsons: data[0]
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        
+        <QuoteCard simpsons={this.state.simpsons} />
+        <button type="button" onClick={this.getSimpsons}>
+          New Quote
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,42 +1,20 @@
-import React from "react";
-import "./QuoteCard.css";
+import React from 'react';
+import './QuoteCard.css';
 
-/* const QuoteCard = (props) => {
-  return (<figure className="QuoteCard">
-    <img src={props.image} alt={props.character} />
-    <figcaption>
-      <blockquote>{props.quote}</blockquote>
-      <cite>{props.character}</cite>
-    </figcaption>
-  </figure>);
-} */
 
-// Le state de "favorite" remonté au parent QuoteAPI,
-// et du même coup, j'ai re-transformé QuoteCard en composant fonctionnel
-// (comme il n'a plus de state, écrire une classe avec constructor est devenu inutile)
-// La fonction mouseClickFav est aussi passée dans QuoteAPI
-// cette fonction est devenue une prop, et est nommée onClick
-// Tous les 'this' et 'state' sont remplacés par 'props'
-// "favorite" se met maintenant à jour dans le composant par rapport au state de QuoteAPI, récupéré via les props
-
-const QuoteCard = props => {
-  return (
-    <figure className="QuoteCard">
-      <img src={props.image} alt={props.character} />
-      <figcaption>
-        <blockquote>{props.quote}</blockquote>
-        <p>
-          <cite>{props.character}</cite>
-          <span
-            className={props.favorite ? "is-favorite" : ""}
-            onClick={props.onClick}
-          >
-            &#9733;
-          </span>
-        </p>
-      </figcaption>
-    </figure>
-  );
+function QuoteCard ({ simpsons }) {
+    return (
+      <figure className="QuoteCard">
+        <img src={simpsons.image} alt={simpsons.character} />
+        <figcaption>
+          <blockquote>{simpsons.quote}</blockquote>
+          <p>
+            <cite>{simpsons.character}</cite>
+            
+          </p>
+        </figcaption>
+      </figure>
+    );
 };
 
 export default QuoteCard;
